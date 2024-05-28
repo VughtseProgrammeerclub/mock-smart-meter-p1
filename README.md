789aca1ef5daf74bc8c50b14f665f828ec968d05
Joep Weijers <joepw@topdesk.com>
2024-05-28 20:45:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Mock Smart Meter to test reading out the P1 port
+
+Standalone version of the [simulator](https://github.com/Josverl/micropython-p1meter/) created by Josverl. Used in a [Smart Home course](https://github.com/VughtseProgrammeerclub/vpc-iot/tree/main/cursusavond6) of the [Vughtse Programmeerclub](https://github.com/VughtseProgrammeerclub/). 
+
+Used on a Raspberry Pi Pico W, connected on TX_Pin GPIO4
---
+++ main.py
@@ -0,0 +1,214 @@
+# pylint: disable=trailing-whitespace
+
+import logging
+import random
+from uctypes import UINT16
+from machine import UART, reset
+import uasyncio as asyncio
+
+# Logging
+log = logging.getLogger('SIMULATION')
+#set level no lower than ..... for this log only
+log.level = min(logging.INFO, logging._level) #pylint: disable=protected-access
+
+VERBOSE = False
+INTERVAL_SIM = 5 #seconds
+TX_PIN_NR = 4
+RX_PIN_NR = 5
+
+#####################################################
+# test rig
+#####################################################
+print(r"""
+ _____ ________  ____   _ _       ___ _____ ___________ 
+/  ___|_   _|  \/  | | | | |     / _ \_   _|  _  | ___ \
+\ `--.  | | | .  . | | | | |    / /_\ \| | | | | | |_/ /
+ `--. \ | | | |\/| | | | | |    |  _  || | | | | |    / 
+/\__/ /_| |_| |  | | |_| | |____| | | || | \ \_/ / |\ \ 
+\____/ \___/\_|  |_/\___/\_____/\_| |_/\_/  \___/\_| \_|
+""")
+
+def crc16(buf: bytearray) -> UINT16:
+    """CRC-16-ANSI calculated over the characters in the data message using the polynomial: x16 + x15 + x2 + 1
+    """
+    # http://www.nodo-domotica.nl/images/8/86/DSMR.pdf
+    # https://en.wikipedia.org/wiki/Cyclic_redundancy_check
+    crc = 0x0000
+    for c in buf:
+        crc ^= c
+        for i in range(8):          # pylint: disable=unused-variable
+            if crc & 0x01:
+                crc >>= 1
+                crc ^= 0xA001
+            else:
+                crc >>= 1
+    return crc
+
+class P1MeterSIM():
+    """
+    P1 meter to fake a Dutch electricity meter and generate some reading to test the rest of the software
+    """
+
+    telegrams_tx: int = 0
+    uart: UART
+
+    def __init__(self):
+        # init port for receiving 115200 Baud 8N1 using inverted polarity in RX/TX
+        # UART 1 = Receive and TX if configured as Splitter
+        self.uart = UART(1, rx=RX_PIN_NR, tx=TX_PIN_NR,
+                         baudrate=115200, bits=8, parity=None,
+                         stop=1, invert=UART.INV_RX | UART.INV_TX,
+                         txbuf=2048, rxbuf=2048)                     # larger buffer for testing and stability
+
+    async def sender(self):
+        """
+        Simulates data being sent from the p1 port to aid in debugging
+        this assumes that pin rx and tx are connected
+        """
+        swriter = asyncio.StreamWriter(self.uart, {})
+        while True:
+            log.warning('send simulated telegram on pin {}'.format(TX_PIN_NR))
+            telegram = self.fake_message()
+            if VERBOSE: 
+                log.debug(b'TX telegram message: '+telegram)
+            swriter.write(telegram)
+            await swriter.drain()       # pylint: disable= not-callable
+            self.telegrams_tx += 1
+            await asyncio.sleep(INTERVAL_SIM)
+
+    @staticmethod
+    def fake_message():
+        # Pick a template
+        #msg = meter1
+        msg = meter3
+
+        u = random.randint(-1000, 1000) /100
+        msg = msg.format(0, max(u, 0), -1*min(u, 0))
+        buf = bytearray(msg, 'utf8')
+        crc_computed = "{0:04X}".format(crc16(buf))
+        # log.debug("TX CRC16 buf : {}".format(buf))
+        log.debug("TX computed CRC {0}".format(crc_computed))
+        msg = msg + "{0}".format(crc_computed) + '\r\n'
+        return msg
+
+# spell-checker: disable 
+
+meter1 = """/XMX5LGBBFG1012650850
+1-3:0.2.8(42)
+1-0:1.7.0({1:06.3f}*kW)
+1-0:2.7.0({2:06.3f}*kW)
+1-0:1.8.1(009248.534*kWh)
+0-1:24.2.1(200909220000S)(05907.828*m3)
+!"""
+
+meter2 = (
+        "/KFM5KAIFA-METER\n"
+        "\n"
+        "1-3:0.2.8(42)\n"
+        "0-0:1.0.0(170124213128W)\n"
+        "0-0:96.1.1(4530303236303030303234343934333135)\n"
+        "1-0:1.8.1(000306.946*kWh)\n"
+        "1-0:1.8.2(000210.088*kWh)\n"
+        "1-0:2.8.1(000000.000*kWh)\n"
+        "1-0:2.8.2(000000.000*kWh)\n"
+        "0-0:96.14.0(0001)\n"
+        "1-0:1.7.0(02.793*kW)\n"
+        "1-0:2.7.0(00.000*kW)\n"
+        "1-0:1.7.0({1:06.3f}*kW)\n"
+        "1-0:2.7.0({2:06.3f}*kW)\n"
+        "0-0:96.7.21(00001)\n"
+        "0-0:96.7.9(00001)\n"
+        "1-0:99.97.0(1)(0-0:96.7.19)(000101000006W)(2147483647*s)\n"
+        "1-0:32.32.0(00000)\n"
+        "1-0:52.32.0(00000)\n"
+        "1-0:72.32.0(00000)\n"
+        "1-0:32.36.0(00000)\n"
+        "1-0:52.36.0(00000)\n"
+        "1-0:72.36.0(00000)\n"
+        "0-0:96.13.1()\n"
+        "0-0:96.13.0()\n"
+        "1-0:31.7.0(003*A)\n"
+        "1-0:51.7.0(005*A)\n"
+        "1-0:71.7.0(005*A)\n"
+        "1-0:21.7.0(00.503*kW)\n"
+        "1-0:41.7.0(01.100*kW)\n"
+        "1-0:61.7.0(01.190*kW)\n"
+        "1-0:22.7.0(00.000*kW)\n"
+        "1-0:42.7.0(00.000*kW)\n"
+        "1-0:62.7.0(00.000*kW)\n"
+        "0-1:24.1.0(003)\n"
+        "0-1:96.1.0(4730303331303033333738373931363136)\n"
+        "0-1:24.2.1(170124210000W)(00671.790*m3)\n"
+        "!"
+    )
+
+
+
+# telegram = """/XMX5LGBBFG1012650850
+# 1-3:0.2.8(42)                                         Version information for P1 output
+# 0-0:1.0.0(200909224846S)                              Date-time stamp of the P1 message
+# 0-0:96.1.1(4530303331303033373235323935313136)        Equipment identifier (device 0)
+# 1-0:1.8.1(009248.534*kWh)                             Meter Reading electricity delivered to client (Tariff 1) in 0,001 kWh
+# 1-0:1.8.2(010048.316*kWh)                             Meter Reading electricity delivered to client (Tariff 2) in 0,001 kWh
+# 1-0:2.8.1(000136.408*kWh)                             Meter Reading electricity delivered by client (Tariff 1) in 0,001 kWh
+# 1-0:2.8.2(000330.023*kWh)                             Meter Reading electricity delivered by client (Tariff 2) in 0,001 kWh
+# 0-0:96.14.0(0002)                                     Tariff indicator electricity. ( 1 / 2)
+# 1-0:1.7.0(00.317*kW)                                  Actual electricity power delivered (+P) in 1 Watt resolution
+# 1-0:2.7.0(00.000*kW)                                  Actual electricity power received (-P) in 1 Watt resolution
+# 0-0:96.7.21(00001)                                    ~~~ Number of power failures in any phase (missing .255)
+# 0-0:96.7.9(00000)                                     ~~~ Number of long power failures in any phase (missing .255)
+# 1-0:99.97.0(0)(0-0:96.7.19)                           ~~~ Power Failure Event Log (long power failures)
+# 1-0:32.32.0(00000)
+# 1-0:32.36.0(00000)
+# 0-0:96.13.1()                                         Text message
+# 0-0:96.13.0()                                         Text message
+# 0-1:24.1.0(003)                                       Device-Type ( device 1 = 003)  (assumed gas meter) 
+# 0-1:96.1.0(4730303332353631323831383834363136)        Equipment identifier (device 1) 
+# 0-1:24.2.1(200909220000S)(05907.828*m3)               0-[1..4] :24.2.1 Last 5-minute value (temperature converted), gas delivered to client in m3, including decimal values and capture time
+# !A7B3                                                 
+# """
+
+
+meter3 = """/XMX5LGBBFG1012463817
+1-3:0.2.8(42)
+0-0:1.0.0(180624024002S)
+0-0:96.1.1(4530303331303033323632373634333136)
+1-0:1.8.1(002200.945*kWh)
+1-0:1.8.2(001961.604*kWh)
+1-0:2.8.1(000000.000*kWh)
+1-0:2.8.2(000000.000*kWh)
+0-0:96.14.0(0001)
+1-0:1.7.0({1:06.3f}*kW)
+1-0:2.7.0({2:06.3f}*kW)
+0-0:96.7.21(00003)
+0-0:96.7.9(00001)
+1-0:99.97.0(1)(0-0:96.7.19)(170214081346W)(0000006334*s)
+1-0:32.32.0(00000)
+1-0:32.36.0(00000)
+0-0:96.13.1()
+0-0:96.13.0()
+1-0:31.7.0(002*A)
+1-0:21.7.0(00.378*kW)
+1-0:22.7.0(00.000*kW)
+0-1:24.1.0(003)
+0-1:96.1.0(4730303235303033333630383535373136)
+0-1:24.2.1(180624020000S)(00968.481*m3)
+!"""
+
+
+
+async def main():
+    asyncio.create_task(P1MeterSIM().sender())
+    while True:
+        await asyncio.sleep(1)
+
+def run():
+    try:
+        log.info('mock p1 meter is starting...')
+        asyncio.run(main())
+    finally:
+        log.info("Clear async loop retained state")
+        asyncio.new_event_loop()  # Clear retained state
+		reset()
+
+run()
